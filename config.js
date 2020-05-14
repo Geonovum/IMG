0b5f25e2d2ffbd38048d1c68da56e9658dcffc58
Linda van den Brink <l.vandenbrink@geonovum.nl>
2020-05-14 14:18:04 +0200

    config CV

--- config.js
+++ config.js
@@ -14,8 +14,8 @@
 var respecConfig = 
 {
   //-- specStatus is verplicht! (activeer 1 van de volgende) --------------------------
-  specStatus: "GN-WV",                // Werk Versie
-  //specStatus: "GN-CV",              // Consultatie Versie
+  //specStatus: "GN-WV",                // Werk Versie
+  specStatus: "GN-CV",              // Consultatie Versie
   //specStatus: "GN-VV",              // Vaststellings Versie
   //specStatus: "GN-DEF",             // Definitieve Versie
   //-- specType is verplicht! (activeer 1 van de volgende) ----------------------------
@@ -31,7 +31,7 @@ var respecConfig =
   //format: "markdown",                 // altijd "markdown"     
   //-- publishDate is verplicht -------------------------------------------------------
   //-- NB: in de werkversie uitzetten, want dan pakt Respec de pushdate ---------------
-  //publishDate: "2017-06-14",  	      // Format is "YYY-MM-DD"
+  publishDate: "2020-05-14",  	      // Format is "YYY-MM-DD"
   //-- de namen van de Editor(s) ------------------------------------------------------
   //-- vul in: per Editor: name:, company:, companyURL: -------------------------------
   editors: 
@@ -53,6 +53,11 @@ var respecConfig =
     }, 
     {
       name:       "Joeri van Es",
+      company:    "Geonovum",
+      companyURL: "https://www.geonovum.nl"
+    }, 
+    {
+      name:       "Linda van den Brink",
       company:    "Geonovum",
       companyURL: "https://www.geonovum.nl"
     }, 
