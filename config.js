ce925f58f890581dc07fbd9bc640e6e643152bfe
Wilko Quak <w.quak@geonovum.nl>
2021-03-25 16:13:51 +0100

    2020 -> 2021

--- config.js
+++ config.js
@@ -31,7 +31,7 @@ var respecConfig =
   //format: "markdown",                 // altijd "markdown"     
   //-- publishDate is verplicht -------------------------------------------------------
   //-- NB: in de werkversie uitzetten, want dan pakt Respec de pushdate ---------------
-  //publishDate: "2020-10-29",  	      // Format is "YYY-MM-DD"
+  publishDate: "2021-03-19",  	      // Format is "YYY-MM-DD"
   //-- de namen van de Editor(s) ------------------------------------------------------
   //-- vul in: per Editor: name:, company:, companyURL: -------------------------------
   editors: 
