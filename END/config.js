1245258ef4159587e73f8591c41699185b0871e6
joerivanes <55691888+joerivanes@users.noreply.github.com>
2021-03-31 12:11:10 +0200

    Update config.js

--- END/config.js
+++ END/config.js
@@ -22,13 +22,13 @@ var respecConfig =
   //-- specType is verplicht! (activeer 1 van de volgende) ----------------------------
   //specType: "NO",                   // Norm
   //specType: "ST",                   // Standaard
-  //specType: "IM",                   // Informatie Model
+specType: "IM",                   // Informatie Model
   //specType: "PR",                   // Praktijk Richtlijn
   //specType: "HR",                   // HandReiking
   //specType: "WA",                   // Werkafspraak
   //specType: "US",                   // Uitwisselings standaard
   //specType: "PS",                   // Presentatie standaard
-  specType: "AL",                   // Algemeen document
+//  specType: "AL",                   // Algemeen document
   //specType: "BD",                   // Beheer documentatie
   //-- format is verplicht! -----------------------------------------------------------
   //format: "markdown",                 // altijd "markdown"     
@@ -106,7 +106,7 @@ var respecConfig =
         publisher:  "naam van de publiceerder",
         company:    "eventueel naam van bedrijf",
     },
-    "MIM": {
+    MIM: {
       id: "mim11",
       title: "MIM - Metamodel Informatie Modellering",
       href: "https://docs.geostandaarden.nl/mim/mim10/",
