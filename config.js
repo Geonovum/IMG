54a11438d873cb073d860989765823ff23665fce
Pieter Bresters <80040145+PB-GNM@users.noreply.github.com>
2021-11-16 14:01:28 +0100

    Update config.js
    laatste edits na publicatie def versie van 15-11-2021

--- config.js
+++ config.js
@@ -14,10 +14,10 @@
 var respecConfig = 
 {
   //-- specStatus is verplicht! (activeer 1 van de volgende) --------------------------
-  //specStatus: "GN-WV",                // Werk Versie
+  specStatus: "GN-WV",                // Werk Versie
   //specStatus: "GN-CV",              // Consultatie Versie
   //specStatus: "GN-VV",              // Vaststellings Versie
-  specStatus: "GN-DEF",             // Definitieve Versie
+  //specStatus: "GN-DEF",             // Definitieve Versie
   //-- specType is verplicht! (activeer 1 van de volgende) ----------------------------
   //specType: "NO",                   // Norm
   //specType: "ST",                     // Standaard
@@ -31,7 +31,7 @@ var respecConfig =
   //format: "markdown",                 // altijd "markdown"     
   //-- publishDate is verplicht -------------------------------------------------------
   //-- NB: in de werkversie uitzetten, want dan pakt Respec de pushdate ---------------
-  publishDate: "2021-11-15",  	      // Format is "YYY-MM-DD"
+  //publishDate: "2021-11-15",  	      // Format is "YYY-MM-DD"
   //-- de namen van de Editor(s) ------------------------------------------------------
   //-- vul in: per Editor: name:, company:, companyURL: -------------------------------
   editors: 
@@ -154,8 +154,8 @@ var respecConfig =
   },
 
   //-- Voor dit blok geldt: alleen als er eerdere versies zijn en altijd beiden aan/uit! 
-  previousPublishDate: "2021-05-21",    	    // Format is "YYYY-MM-DD"
-  //previousMaturity: "GN-DEF",                 // kies 1 van deze 3 regels
+  previousPublishDate: "2021-11-15",    	    // Format is "YYYY-MM-DD"
+  //previousMaturity: "GN-VV",                 // kies 1 van deze 3 regels
   //previousMaturity: "GN-CV",                   // kies 1 van deze 3 regels  	  
   previousMaturity: "GN-DEF",  	                // kies 1 van deze 3 regels
 
