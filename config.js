9587f522d077e2e0ba0f43dd3fd9766ed2c73091
Wilko Quak <w.quak@geonovum.nl>
2022-07-12 14:55:15 +0200

    publicatiedatum aangepast

--- config.js
+++ config.js
@@ -31,7 +31,7 @@ var respecConfig =
   //format: "markdown",                 // altijd "markdown"     
   //-- publishDate is verplicht -------------------------------------------------------
   //-- NB: in de werkversie uitzetten, want dan pakt Respec de pushdate ---------------
-  publishDate: "2022-07-08",  	      // Format is "YYY-MM-DD"
+  publishDate: "2022-07-05",  	      // Format is "YYY-MM-DD"
   //-- de namen van de Editor(s) ------------------------------------------------------
   //-- vul in: per Editor: name:, company:, companyURL: -------------------------------
   editors: 
