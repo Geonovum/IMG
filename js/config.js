75e1356e0df961931691a8de25723c12b27011d1
Pieter Bresters <80040145+PB-GNM@users.noreply.github.com>
2024-07-11 11:14:10 +0200

    Update config.js
    tijdelijk voor RC

--- js/config.js
+++ js/config.js
@@ -4,8 +4,8 @@ let respecConfig = {
   // title is verplicht! Neem hier de titel van het document op ----------------------
   title: "Informatie Model Geluid (IMGeluid) versie 3.1.1",
   //-- specStatus is verplicht! (activeer 1 van de volgende) --------------------------
-  specStatus: "wv",                   // Werkversie
-  //specStatus: "cv",               // Consultatieversie
+  //specStatus: "wv",                   // Werkversie
+  specStatus: "cv",               // Consultatieversie
   //specStatus: "vv",               // Versie ter vaststelling
   //specStatus: "def",              // Vastgestelde versie
   //specStatus: "basis",            // Basis Document
@@ -21,7 +21,7 @@ let respecConfig = {
   
 
   //-- NB: in de werkversie uitzetten, want dan pakt Respec de pushdate ---------------
-  //publishDate: "2024-07-10",
+  publishDate: "2024-07-11",
   //eventueel is het mogelijk een versienummer mee te geven, maar bij Geonovum werken we gewoonlijk alleen met datum als onderdeel van de permanente URI.
   //publishVersion: "0.0.2",
   //previousVersion: "0.0.1",
