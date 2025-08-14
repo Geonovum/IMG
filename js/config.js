770bf0980db5b1a137bc846bef009a441e5f11af
Wilko Quak <w.quak@geonovum.nl>
2025-08-14 13:00:30 +0200

    na een publicatie moet de workflow weer werkversie worden.

--- js/config.js
+++ js/config.js
@@ -4,10 +4,10 @@ let respecConfig = {
   // title is verplicht! Neem hier de titel van het document op ----------------------
   title: "Informatie Model Geluid (IMGeluid) versie 3.1.1",
   //-- specStatus is verplicht! (activeer 1 van de volgende) --------------------------
-  //specStatus: "wv",                   // Werkversie
+  specStatus: "wv",                   // Werkversie
   //specStatus: "cv",               // Consultatieversie
   //specStatus: "vv",               // Versie ter vaststelling
-  specStatus: "def",              // Vastgestelde versie
+  //specStatus: "def",              // Vastgestelde versie
   //specStatus: "basis",            // Basis Document
   //-- specType is verplicht bij alle andere dan BASIS ---------------------------------
   specType: "IM",                 // Informatie Model
@@ -21,12 +21,12 @@ let respecConfig = {
   
 
   //-- NB: in de werkversie uitzetten, want dan pakt Respec de pushdate ---------------
-  publishDate: "2024-08-22",
+  //publishDate: "",
   //eventueel is het mogelijk een versienummer mee te geven, maar bij Geonovum werken we gewoonlijk alleen met datum als onderdeel van de permanente URI.
   //publishVersion: "0.0.2",
   //previousVersion: "0.0.1",
   //-- Voor dit blok geldt: alleen als er eerdere versies zijn en altijd beiden aan/uit! 
-  previousPublishDate: "2023-10-06",
+  previousPublishDate: "2024-08-22",
   previousMaturity: "DEF",
   //-- vul in: per Editor: name:, company:, companyURL: -------------------------------
   editors: 
