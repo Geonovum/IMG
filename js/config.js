8c4359f75525e98e8640d73517e186898c96f84e
Wilko Quak <w.quak@geonovum.nl>
2026-09-09 17:13:27 +0200

    nog even werkversie en beoogde publicatiedatum ingevuld

--- js/config.js
+++ js/config.js
@@ -4,9 +4,9 @@ let respecConfig = {
   // TODO: title is verplicht! Neem hieronder de titel van het document op
   title: "Informatie Model Geluid (IMGeluid)",
   //-- specStatus is verplicht! (activeer 1 van de volgende) --------------------------
-  //specStatus: "wv",                   // Werkversie
+  specStatus: "wv",                   // Werkversie
   //specStatus: "cv",               // Consultatieversie
-  specStatus: "vv",               // Versie ter vaststelling
+  //specStatus: "vv",               // Versie ter vaststelling
   //specStatus: "def",              // Vastgestelde versie
   //specStatus: "basis",            // Basis Document
 
@@ -25,7 +25,7 @@ let respecConfig = {
   
   //-- publishDate is verplicht -------------------------------------------------------
   //-- NB: in de werkversie uitzetten, want dan pakt Respec de pushdate ---------------
-  publishDate: "2026-09-30",
+  publishDate: "2026-09-25",
   //eventueel is het mogelijk een versienummer mee te geven, maar bij Geonovum werken we gewoonlijk alleen met datum als onderdeel van de permanente URI.
   publishVersion: "4.0",
   previousPublishVersion: "3.1.1",
